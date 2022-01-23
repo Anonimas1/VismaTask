82b5b67650b7126a2bde40d738bed75233328dfc
Audrius Puzinas <puzinas.audrius@gmail.com>
2022-01-23 23:38:02 +0200

    Add entry point for the app

--- VismaTask/Program.cs
+++ VismaTask/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using VismaTask.Controllers;
+using VismaTask.Models.Meetings;
+using VismaTask.Repositories.Meetings;
+using VismaTask.Services.Meetings;
+using VismaTask.Views;
 
 namespace VismaTask;
 
@@ -6,6 +13,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string user = GetName();
+        IMeetingRepository repository = new MeetingRepository("meetings.json");
+        IMeetingService meetingService = new MeetingService();
+        
+        var meetingController = new MeetingController(repository, meetingService, user);
+        
+        MainView mainView = new MainView(meetingController);
+        mainView.ShowView();
+    }
+
+    public static string GetName()
+    {
+        string user;
+        while (true)
+        {
+            Console.WriteLine("Enter your name:");
+            user = Console.ReadLine();
+            if (user.Length > 0)
+                break;
+        }
+
+        return user;
     }
 }
