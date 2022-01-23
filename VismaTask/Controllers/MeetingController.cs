c8a3a1571a7729b6d295add2b2f5a91076374737
Audrius Puzinas <puzinas.audrius@gmail.com>
2022-01-23 14:27:07 +0200

    Implement GetAll method

---
+++ VismaTask/Controllers/MeetingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using VismaTask.Models.Meetings;
+using VismaTask.Repositories.Meetings;
+
+namespace VismaTask.Controllers;
+
+public class MeetingController
+{
+    private readonly IMeetingRepository _meetingRepository;
+    
+    public MeetingController(IMeetingRepository meetingRepository)
+    {
+        _meetingRepository = meetingRepository;
+    }
+    
+    
+    public List<Meeting> GetAll()
+    {
+        return _meetingRepository.GetAll();
+    }
+}
