89a8b117a0fa54cb53bca163c2529d6a184dd156
Audrius Puzinas <puzinas.audrius@gmail.com>
2022-01-23 16:51:37 +0200

    Create meeting repository stub

---
+++ VismaTaskTest/Stubs/MeetingRepositoryStub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using VismaTask.Models.Meetings;
+using VismaTask.Repositories.Meetings;
+
+namespace VismaTaskTest.Stubs;
+
+public class MeetingRepositoryStub : IMeetingRepository
+{
+    public List<Meeting> GetAll()
+    {
+        return new List<Meeting>()
+        {
+            new Meeting(
+                "First",
+                "First responsible",
+                "First description",
+                MeetingCategory.Hub,
+                MeetingType.Live,
+                new DateTime(2000, 01, 01),
+                new DateTime(2000, 01, 02)
+                ),
+            new Meeting(
+                "Second",
+                "Second responsible",
+                "Second description",
+                MeetingCategory.Short,
+                MeetingType.Live,
+                new DateTime(2001, 01, 01),
+                new DateTime(2001, 01, 02)
+            ),
+            new Meeting(
+                "Third",
+                "Third responsible",
+                "Third description",
+                MeetingCategory.Short,
+                MeetingType.Live,
+                new DateTime(2002, 01, 01),
+                new DateTime(2002, 01, 02)
+            ),
+            new Meeting(
+                "Fourth",
+                "Fourth responsible",
+                "Fourth description",
+                MeetingCategory.Short,
+                MeetingType.InPerson,
+                new DateTime(2002, 01, 01),
+                new DateTime(2002, 01, 02)
+            )
+        };
+
+    }
+
+    public Meeting Create(Meeting meetingToAdd)
+    {
+        return meetingToAdd;
+    }
+}
