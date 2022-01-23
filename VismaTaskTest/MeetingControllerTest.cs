259d2280f475a116caaff948855cb886b29c36df
Audrius Puzinas <puzinas.audrius@gmail.com>
2022-01-23 18:47:11 +0200

    Refractor controller tests

--- VismaTaskTest/MeetingControllerTest.cs
+++ VismaTaskTest/MeetingControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using VismaTask.Controllers;
 using VismaTask.Models.Meetings;
@@ -7,17 +8,27 @@ using Xunit;
 
 namespace VismaTaskTest;
 
-public class MeetingControllerTest
+public class MeetingControllerTest : IDisposable
 {
+    private MeetingController _controller;
+    
+    public MeetingControllerTest()
+    {
+        _controller = new MeetingController(new MeetingRepositoryStub(), "Admin");
+    }
+
+    public void Dispose()
+    {
+    }
+    
     [Fact]
     public void GetAll_NoFilterSet_ReturnsAllElements()
     {
         var repositoryStub = new MeetingRepositoryStub();
-        var controller = new MeetingController(repositoryStub);
         var filter = new MeetingFilter();
         var listFormRepository = repositoryStub.GetAll();
         
-        var actual = controller.GetAll(filter);
+        var actual = _controller.GetAll(filter);
 
         Assert.Equal(listFormRepository.Count, actual.Count);
     }
@@ -26,10 +37,9 @@ public class MeetingControllerTest
     public void GetAll_FilterSet_ReturnsZeroElements()
     {
         var repositoryStub = new MeetingRepositoryStub();
-        var controller = new MeetingController(repositoryStub);
         var filter = new MeetingFilter(){NumberOfAttendees = 1};
 
-        var actual = controller.GetAll(filter);
+        var actual = _controller.GetAll(filter);
 
         Assert.Empty(actual);
     }
@@ -38,13 +48,13 @@ public class MeetingControllerTest
     public void GetAll_FilterSet_ReturnsFilteredElements()
     {
         var repositoryStub = new MeetingRepositoryStub();
-        var controller = new MeetingController(repositoryStub);
         var filter = new MeetingFilter(){Category = MeetingCategory.Short};
         var listFormRepository = repositoryStub.GetAll().
             Where(m => m.Category == MeetingCategory.Short).
             ToList();
 
-        var actual = controller.GetAll(filter);
+        var actual = _controller.GetAll(filter);
         Assert.Equal(listFormRepository.Count, actual.Count);
     }
+
 }
